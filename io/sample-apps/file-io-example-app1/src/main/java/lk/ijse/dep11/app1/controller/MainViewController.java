474be751850f3afd2d8d84b81a71125c97f25879
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-09-21 16:20:09 +0530

    Finish till File I/O

--- io/sample-apps/file-io-example-app1/src/main/java/lk/ijse/dep11/app1/controller/MainViewController.java
+++ io/sample-apps/file-io-example-app1/src/main/java/lk/ijse/dep11/app1/controller/MainViewController.java
@@ -1,8 +1,11 @@
 package lk.ijse.dep11.app1.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
 import lk.ijse.dep11.app1.tm.Employee;
 
 import java.util.List;
@@ -16,6 +19,7 @@ public class MainViewController {
     public TableView<Employee> tblEmployee;
     public TextField txtSearch;
     public Button btnNew;
+    public AnchorPane root;
 
     public void initialize(){
         for (Control control : new Control[]{txtId, txtName, txtContact, btnSave, btnDelete}) {
@@ -25,6 +29,31 @@ public class MainViewController {
         tblEmployee.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
         tblEmployee.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
         tblEmployee.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("contact"));
+
+        tblEmployee.getSelectionModel().selectedItemProperty().addListener((o, old, current)->{
+            if (current == null){
+                btnDelete.setDisable(true);
+            }else{
+                txtId.setText(current.getId());
+                txtName.setText(current.getName());
+                txtContact.setText(current.getContact());
+                btnDelete.setDisable(false);
+            }
+        });
+
+        tblEmployee.setOnKeyPressed(event ->  {
+            if (event.getCode() == KeyCode.DELETE) btnDelete.fire();
+        });
+
+        Platform.runLater(()->{
+            root.getScene().getWindow().setOnCloseRequest(event -> {
+                saveEmployeeList();
+            });
+        });
+    }
+
+    private void saveEmployeeList(){
+
     }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
@@ -35,6 +64,7 @@ public class MainViewController {
 
         /* Business Validation */
         for (Employee employee : employeeList) {
+            if (tblEmployee.getSelectionModel().getSelectedItem() == employee) continue;
             if (employee.getContact().equals(txtContact.getText())){
                 new Alert(Alert.AlertType.ERROR, "Contact number already exists").show();
                 txtContact.requestFocus();
@@ -43,10 +73,21 @@ public class MainViewController {
             }
         }
 
-        Employee newEmployee = new Employee(txtId.getText(),
-                txtName.getText().strip(), txtContact.getText().strip());
-        employeeList.add(newEmployee);
-        btnNew.fire();
+        if (tblEmployee.getSelectionModel().isEmpty()){
+            /* New Record */
+            Employee newEmployee = new Employee(txtId.getText(),
+                    txtName.getText().strip(), txtContact.getText().strip());
+            employeeList.add(newEmployee);
+            btnNew.fire();
+        } else {
+            /* Update */
+            Employee selectedEmployee = tblEmployee.getSelectionModel().getSelectedItem();
+            selectedEmployee.setName(txtName.getText().strip());
+            selectedEmployee.setContact(txtContact.getText().strip());
+            tblEmployee.refresh();
+            btnNew.fire();
+        }
+
     }
 
     private List<Employee> getEmployeeList(){
@@ -67,6 +108,9 @@ public class MainViewController {
     }
 
     public void btnDeleteOnAction(ActionEvent actionEvent) {
+        Employee selectedEmployee = tblEmployee.getSelectionModel().getSelectedItem();
+        getEmployeeList().remove(selectedEmployee);
+        if (getEmployeeList().isEmpty()) btnNew.fire();
     }
 
     public void btnNewOnAction(ActionEvent actionEvent) {
@@ -76,6 +120,7 @@ public class MainViewController {
             control.setDisable(false);
         }
         txtName.requestFocus();
+        tblEmployee.getSelectionModel().clearSelection();
     }
 
     private String getNewEmployeeId(){
